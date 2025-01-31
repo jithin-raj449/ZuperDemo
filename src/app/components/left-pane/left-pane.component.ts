b00125454ee1ee5c88ef1827d6e3f45c0fc536cb
jithin-raj449 <jithin.raj449@gmail.com>
2025-01-31 12:46:39 -0500

    left pane

--- src/app/components/left-pane/left-pane.component.ts
+++ src/app/components/left-pane/left-pane.component.ts
@@ -1,11 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+interface FieldGroup {
+  id: number;
+  name: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-left-pane',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './left-pane.component.html',
-  styleUrl: './left-pane.component.scss'
+  styleUrl: './left-pane.component.scss',
 })
 export class LeftPaneComponent {
+  fieldGroups: FieldGroup[] = [
+    {
+      id: 1,
+      name: 'Defualt field group',
+      description: '',
+    },
+  ];
+  newGroupName: string = '';
+  newGroupDescription: string = '';
+  constructor(private fb: FormBuilder) {
+    this.loadFieldGroups();
+  }
 
+  loadFieldGroups() {
+    const savedGroups = localStorage.getItem('fieldGroups');
+    if (savedGroups) {
+      this.fieldGroups = JSON.parse(savedGroups);
+    }
+  }
+
+  saveFieldGroups() {
+    // localStorage.setItem('fieldGroups', JSON.stringify(this.fieldGroups));
+  }
+
+  addFieldGroup() {
+    const groupCount = this.fieldGroups.length;
+    const newGroupName = `Form Group ${groupCount}`;
+
+    const newGroup: FieldGroup = {
+      id: Date.now(),
+      name: newGroupName,
+      description: '',
+    };
+
+    this.fieldGroups.push(newGroup);
+    this.saveFieldGroups();
+  }
+
+  deleteFieldGroup(id: number) {
+    this.fieldGroups = this.fieldGroups.filter((group) => group.id !== id);
+    this.saveFieldGroups();
+  }
 }
